9161c849e2e2daa07142ed96fb513fa1e4e9a6c3
Tyler <trapgar@gmail.com>
2024-04-18 22:34:00 -0500

    initial commit

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+export const extensionName = 'gamedevjs-ui';
+export const sceneExplorerId = 'sceneExplorer';
+export const sceneOutlinerId = 'sceneOutliner';
+export const objectDetailsId = 'objectDetails';
